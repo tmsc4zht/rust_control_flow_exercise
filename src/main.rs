78e2649ceaed7010ff726a6f265eb32fa6545dc6
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-03-03 19:14:45 +0900

    簡略化

--- src/main.rs
+++ src/main.rs
@@ -37,47 +37,38 @@ fn fibonacci_number(n: u32) -> u32 {
 }
 
 fn the_twelve_days_of_christmas() {
-    for i in 0..12 {
-        println!("{}.", i + 1);
-        n_day_of_christmas(i);
-        println!("");
-    }
-}
-
-fn n_day_of_christmas(n: usize) {
     // https://genius.com/Christmas-songs-the-twelve-days-of-christmas-lyrics
     let ordinal_numeral = [
         "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
         "tenth", "eleventh", "twelfth",
     ];
 
-    let presents = [
-        "A partridge in a pear tree",
-        "Two turtle doves",
-        "Three french hens",
-        "Four calling birds",
-        "Five golden rings",
-        "Six geese a-laying",
-        "Seven swans a-swimming",
-        "Eight maids a-milking",
-        "Nine ladies dancing",
-        "Ten lords a-leaping",
-        "Eleven pipers piping",
+    let lyric_lines = [
         "Twelve drummers drumming",
+        "Eleven pipers piping",
+        "Ten lords a-leaping",
+        "Nine ladies dancing",
+        "Eight maids a-milking",
+        "Seven swans a-swimming",
+        "Six geese a-laying",
+        "Five golden rings",
+        "Four calling birds",
+        "Three french hens",
+        "Two turtle doves, and",
+        "A partridge in a pear tree",
     ];
 
-    let presents = &presents[0..n + 1];
+    for i in 0..12 {
+        println!("{}.", i + 1);
+        println!("On the {} day of Christmas,", ordinal_numeral[i]);
+        println!("my true love sent to me");
 
-    println!("On the {} day of Christmas,", ordinal_numeral[n]);
-    println!("my true love sent to me");
-
-    if presents.len() >= 3 {
-        for idx in (2..presents.len()).rev() {
-            println!("{}", presents[idx])
+        // n = 0 => from 11 to 11
+        // n = 1 => from 10 to 11
+        // n = 11 => from 0 to 11
+        for lyric_line in lyric_lines[(11 - i)..].iter() {
+            println!("{}", lyric_line);
         }
+        println!("");
     }
-    if presents.len() >= 2 {
-        println!("{}, and", presents[1]);
-    }
-    println!("{}", presents[0]);
 }
