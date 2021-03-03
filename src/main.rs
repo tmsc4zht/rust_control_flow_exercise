1f603b4936da6fc5fbd8637fae71d1abd5a1ed36
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-03-03 17:33:41 +0900

    摂氏華氏変換関数の実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 0.0;
+    println!("{} °C is {} °F", x, celsius_to_fahrenheit(x));
+    println!("{} °F is {} °C", x, fahrenheit_to_celsius(x));
+
+    let x = 100.0;
+    println!("{} °C is {} °F", x, celsius_to_fahrenheit(x));
+    println!("{} °F is {} °C", x, fahrenheit_to_celsius(x));
+}
+
+fn celsius_to_fahrenheit(celcius: f32) -> f32 {
+    9.0 / 5.0 * celcius + 32.0
+}
+
+fn fahrenheit_to_celsius(fahrenheit: f32) -> f32 {
+    5.0 / 9.0 * (fahrenheit - 32.0)
 }
